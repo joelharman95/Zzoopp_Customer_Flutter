8734fad5469386837e942aab426ca3f65c3e2771
Nethaji <50766400+joelharman95@users.noreply.github.com>
2021-09-03 08:06:21 +0530

    Changed baseurl

--- lib/app/parameters/prod_parameters.dart
+++ lib/app/parameters/prod_parameters.dart
@@ -7,8 +7,8 @@ class ProdParameter extends BaseParameter {
             appName: "Zzoopp Food",
             clientId: "",
             clientSecret: "",
-            gmapWebKey: "AIzaSyDNtNt5hQpHyYe4t_0cnOw5Pd8VgtEfxdc",
-            baseUrl: "https://1d1.storenxt.in/api/customer/");
+            gmapWebKey: "AIzaSymdsklfngwrnoiewfewonsdsgs",
+            baseUrl: "https://india.store.in/api/customer/");
 
   @override
   Flavor get flavor => Flavor.production;
