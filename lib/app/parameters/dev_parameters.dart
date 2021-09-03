bdca5447b79c8ab8cba39f1a28feb00ccd40cc94
Nethaji <50766400+joelharman95@users.noreply.github.com>
2021-09-03 08:06:53 +0530

    Changed baseUrl

--- lib/app/parameters/dev_parameters.dart
+++ lib/app/parameters/dev_parameters.dart
@@ -6,8 +6,8 @@ class DevParameter extends BaseParameter {
             appName: "Zzoopp Food DEV",
             clientId: "",
             clientSecret: "",
-            gmapWebKey: "AIzaSyDNtNt5hQpHyYe4t_0cnOw5Pd8VgtEfxdc",
-            baseUrl: "https://1d1.storenxt.in/api/customer/");
+            gmapWebKey: "AIzaSymdsklfngwrnoiewfewonsdsgs",
+            baseUrl: "https://india.store.in/api/customer/");
 
   @override
   Flavor get flavor => Flavor.development;
